292a75236b7735894e489f35e912805030c8ee25
gabskit <gaby.mofa31@gmail.com>
2026-03-14 12:49:05 -0600

    update

---
+++ www/main.js
@@ -0,0 +1,25 @@
+
+let filerece = []
+
+document.addEventListener('alpine:init', () => {
+	Alpine.data('inicio', () => ({
+		hili: ["ui-btn-active ui-state-persist","","","",""],
+		renderNav() {
+		 return `
+			<div data-role="navbar" data-iconpos="top">
+				<ul>
+					<li><a @click="navi(0)" :class="hili[0]" href="#main" data-icon="home">Inicio</a></li>
+					<li><a @click="navi(1)" :class="hili[1]" href="#notas" data-icon="nota">Notas</a></li>
+					<li><a @click="navi(2)" :class="hili[2]" href="#calen" data-icon="calen">Calendario</a></li>
+					<!--li><a @click="navi(3)" :class="hili[3]" href="#busca" data-icon="search">Buscar</a></li-->
+					<li><a @click="navi(4)" :class="hili[4]" href="#ajustes" data-icon="gear">Ajustes</a></li>
+				</ul>
+			</div>`
+		},
+		navi(index){
+		 this.hili = ["","","","",""]
+		 this.hili[index] = "ui-btn-active ui-state-persist"
+		},
+		recent: filerece
+	}))
+ })
